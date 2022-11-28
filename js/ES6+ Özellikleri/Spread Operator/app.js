5aedf81bd9272619fd4d7484aad0e8c2039dac6a
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2022-11-28 21:54:26 +0300

    Spread Operator Kullanımı

---
+++ js/ES6+ Özellikleri/Spread Operator/app.js
@@ -0,0 +1,19 @@
+//Spread Operator
+//-----------------------------------------------------------
+// üç nokta kullanarak tüm elemanları yazdırabiliriz
+//const langs = ["Python", "C++","Java"];
+//console.log(...langs);
+// yeni bir dizi içerisine üç nokta ile ekleme yapabiliriz
+//const langs2 = ["JavaScript", ...langs];
+//------------------------------------------------------------------
+//const numbers = [1,2,3,4,5,6,7,8,9];
+////ilk iki eleman sonrasında geriye kalan elemanları yeni bir diziye atayabiliriz
+//const [a,b,...number2] = numbers;
+//-------------------------------------------------------------------------
+// arrow func oluşturduk. numbers dizisi içerisinde bulunan elemanları
+//a,b,c elemanları ile eşleştirmek istiyoruz ve fonskiyon içerisine
+//spread operator olarak gönderim yaptığımızda dizinin elemanlarını
+//otomatik olarak eşleştirir
+//const addNumbers =(a,b,c) => console.log(a+b+c);
+//const numbers = [100,200,300];
+//addNumbers(...numbers);
